9fc39ed7b05060afc29e851e8e57077703b9a037
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-04-11 17:42:56 +0530

    Moved frontend inside the backend folder for easy management and deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 const postRoutes = require('./routes/postRoutes');
 const authRoutes = require('./routes/authRoutes');
 const commentRoutes = require('./routes/commentRoutes');
-
 
 dotenv.config();
 connectDB();
@@ -14,13 +14,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
 app.use('/posts', postRoutes);
 app.use('/auth', authRoutes);
 app.use('/comments', commentRoutes);
+
+const __dirnamePath = path.resolve();
+app.use(express.static(path.join(__dirnamePath, 'frontend', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirnamePath, 'frontend', 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
